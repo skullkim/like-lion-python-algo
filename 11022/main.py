28fd227f23e8ab402a99a9ff78c93787dd477097
skullKim <space1878@naver.com>
2021-05-21 14:51:01 +0900

    11022

---
+++ 11022/main.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def main():
+    test_cases = int(sys.stdin.readline().strip())
+    for i in range(1, test_cases + 1):
+        num1, num2 = sys.stdin.readline().strip().split()
+        print("Case ", '#', i, ': ', num1, ' + ', num2, ' = ', int(num1) + int(num2), sep='')
+
+
+if __name__ == '__main__':
+    main()
